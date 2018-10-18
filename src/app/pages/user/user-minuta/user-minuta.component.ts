7d21049617af4df8ec040766e3770b3b21a0bb66
Daniel Marconi <daniel.marconi@despegar.com>
2018-10-18 16:42:25 -0300

    Creo el servicio y genero la vista de inicio de las minutas

--- src/app/pages/user/user-minuta/user-minuta.component.ts
+++ src/app/pages/user/user-minuta/user-minuta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MinutaService} from '../../../providers/consorcio/minuta/minuta.service';
 
 @Component({
   selector: 'app-user-minuta',
@@ -8,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class UserMinutaComponent implements OnInit {
 
   title = 'Resúmenes mensuales';
+  meses: string[] = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  minutas: any[];
 
-  constructor() { }
+  constructor(private _minutaService: MinutaService) {
+  }
 
   ngOnInit() {
+      this._minutaService.list()
+          .subscribe(
+              (resp: any) => {
+                  console.log('data.body');
+                  console.log(resp.body);
+                  this.minutas = resp.body.data;
+                  console.log('minutas');
+                  console.log(this.minutas);
+              },
+              error => console.error(error)
+          );
   }
 
 }
