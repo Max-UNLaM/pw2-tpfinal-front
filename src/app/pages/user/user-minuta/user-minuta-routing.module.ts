0bdcb7ff90baa6ec595bfb57afcd630f938231e7
Daniel Marconi <daniel.marconi@despegar.com>
2018-10-18 16:52:55 -0300

    Cambios menores

--- src/app/pages/user/user-minuta/user-minuta-routing.module.ts
+++ src/app/pages/user/user-minuta/user-minuta-routing.module.ts
@@ -12,6 +12,10 @@ const routes: Routes = [
             {
                 path: '',
                 component: UserMinutaComponent
+            },
+            {
+                path: '/:id',
+                component: UserMinutaComponent
             }
         ]
     }
